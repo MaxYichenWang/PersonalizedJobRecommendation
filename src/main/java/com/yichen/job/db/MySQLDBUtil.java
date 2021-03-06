ae0d4fac93088cfc582a208780c9e874ffd58454
MaxYichenWang <59940509+MaxYichenWang@users.noreply.github.com>
2021-03-05 21:33:17 -0800

    Modified AWS account information.

--- src/main/java/com/yichen/job/db/MySQLDBUtil.java
+++ src/main/java/com/yichen/job/db/MySQLDBUtil.java
@@ -3,11 +3,11 @@ package com.yichen.job.db;
 // implement JDBC API
 public class MySQLDBUtil {
     // import Connectivity & security Info from AWS RDS
-    private static final String INSTANCE = "yichen-instance.cahdl1gftf0t.us-east-2.rds.amazonaws.com";
+    private static final String INSTANCE = "";
     private static final String PORT_NUM = "3306";
-    public static final String DB_NAME = "githubjob";
-    private static final String USERNAME = "admin";
-    private static final String PASSWORD = "0487561kao";
+    public static final String DB_NAME = "";
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
     public static final String URL = "jdbc:mysql://"
             + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
             + "?user=" + USERNAME + "&password=" + PASSWORD
